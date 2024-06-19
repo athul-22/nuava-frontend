6e2c49344526426c5b5ac2e89efd0348b1b70131
Athul Nambiar <dev.athulnambiar@gmail.com>
2024-06-19 22:45:40 +0530

    UI Updates

--- src/components/Coach/SendCoachEmailOTP.tsx
+++ src/components/Coach/SendCoachEmailOTP.tsx
@@ -61,9 +61,13 @@ const SendCoachEmailOTP: React.FC = () => {
 
   const [email, setEmail] = useState("");
   const [passkey, setPasskey] = useState("");
-  const [verificationCode, setVerificationCode] = useState<string[]>(Array(6).fill(""));
-  const [sendOTP, { loading: loadingSendOTP, error: errorSendOTP }] = useMutation(SEND_COACH_EMAIL_OTP);
-  const [verifyOTP, { loading: loadingVerifyOTP, error: errorVerifyOTP }] = useMutation(VERIFY_COACH_EMAIL_OTP);
+  const [verificationCode, setVerificationCode] = useState<string[]>(
+    Array(6).fill("")
+  );
+  const [sendOTP, { loading: loadingSendOTP, error: errorSendOTP }] =
+    useMutation(SEND_COACH_EMAIL_OTP);
+  const [verifyOTP, { loading: loadingVerifyOTP, error: errorVerifyOTP }] =
+    useMutation(VERIFY_COACH_EMAIL_OTP);
   const [showOTPForm, setShowOTPForm] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
@@ -101,7 +105,9 @@ const SendCoachEmailOTP: React.FC = () => {
         notyf.success("Email sent successfully");
         setShowOTPForm(true);
       } else {
-        notyf.error(response.data?.sendCoachEmailOTP.message || "Error occurred");
+        notyf.error(
+          response.data?.sendCoachEmailOTP.message || "Error occurred"
+        );
       }
     } catch (err) {
       console.error("Error:", err);
@@ -109,7 +115,10 @@ const SendCoachEmailOTP: React.FC = () => {
     }
   };
 
-  const handleVerificationCodeChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleVerificationCodeChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
     const newCode = [...verificationCode];
     newCode[index] = e.target.value;
     setVerificationCode(newCode);
@@ -142,11 +151,16 @@ const SendCoachEmailOTP: React.FC = () => {
 
       if (response.data?.verifyCoachEmailOTP.status) {
         notyf.success("Email verified successfully");
-        localStorage.setItem("tempToken", response.data.verifyCoachEmailOTP.token);
+        localStorage.setItem(
+          "tempToken",
+          response.data.verifyCoachEmailOTP.token
+        );
         localStorage.setItem("email", email);
         window.location.href = "/coach/success";
       } else {
-        notyf.error(response.data?.verifyCoachEmailOTP.message || "Error occurred");
+        notyf.error(
+          response.data?.verifyCoachEmailOTP.message || "Error occurred"
+        );
       }
     } catch (err) {
       console.error("Error:", err);
@@ -167,43 +181,93 @@ const SendCoachEmailOTP: React.FC = () => {
       <div className="right-column">
         <div className="welcome-container">
           <div className="welcome-text">
-            <p className="welcome-subtitle">Welcome to</p>
+            <p className="welcome-subtitle">Welcome to </p>
             <p className="welcome-title">Nuava Sports</p>
           </div>
 
-          <div className={`form-container ${isMobile ? "mobile-form" : "desktop-form"} ${showOTPForm ? "otp-shown" : ""}`}>
+          <div
+            className={`form-container ${
+              isMobile ? "mobile-form" : "desktop-form"
+            } ${showOTPForm ? "otp-shown" : ""}`}
+          >
             {!showOTPForm ? (
-              <form className="send-otp-form" onSubmit={handleSubmit}>
-                <div className="input-group">
-                  <input
-                    type="email"
-                    id="email"
-                    className="input-field"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-
-                <div className="input-group">
-                  <input
-                    type="text"
-                    id="passkey"
-                    className="input-field"
-                    placeholder="Passkey"
-                    value={passkey}
-                    onChange={(e) => setPasskey(e.target.value)}
-                  />
-                </div>
-
-                <button type="submit" className="submit-button" disabled={loadingSendOTP}>
-                  {loadingSendOTP ? "Sending..." : "Send OTP"}
-                </button>
-              </form>
+             <form className="send-otp-form" onSubmit={handleSubmit}>
+             <div className="input-group">
+               <div className="input-icon">
+                 {/* <svg
+                   xmlns="http://www.w3.org/2000/svg"
+                   fill="none"
+                   viewBox="0 0 24 24"
+                   strokeWidth="1.5"
+                   stroke="currentColor"
+                   className="size-6 icon"
+                 >
+                   <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                   />
+                 </svg> */}
+                 <input
+                   type="email"
+                   id="email"
+                   className="input-field"
+                   placeholder="Email"
+                   value={email}
+                   onChange={(e) => setEmail(e.target.value)}
+                 />
+               </div>
+             </div>
+           
+             <div className="input-group">
+               <div className="input-icon">
+                 {/* <svg
+                   xmlns="http://www.w3.org/2000/svg"
+                   fill="none"
+                   viewBox="0 0 24 24"
+                   strokeWidth="1.5"
+                   stroke="currentColor"
+                   className="size-6 icon"
+                 >
+                   <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"
+                   />
+                 </svg> */}
+                 <input
+                   type="text"
+                   id="passkey"
+                   className="input-field"
+                   placeholder="Passkey"
+                   value={passkey}
+                   onChange={(e) => setPasskey(e.target.value)}
+                 />
+               </div>
+             </div>
+           
+             <button
+               type="submit"
+               className="submit-button"
+               disabled={loadingSendOTP}
+             >
+               {loadingSendOTP ? "Sending..." : "Send OTP"}
+             </button>
+           </form>
+           
             ) : (
               <div className="otp-animation-container">
                 <div className="otp-info">
-                  <p style={{ color: 'green', fontSize: '18px', marginBottom: '-30px', textAlign: 'left' }}>A 6 digit OTP sent to {email}</p>
+                  <p
+                    style={{
+                      color: "green",
+                      fontSize: "18px",
+                      marginBottom: "-30px",
+                      textAlign: "left",
+                    }}
+                  >
+                    A 6 digit OTP sent to {email}
+                  </p>
                 </div>
                 <div className="verification-code-container">
                   {verificationCode.map((_, index) => (
@@ -218,7 +282,11 @@ const SendCoachEmailOTP: React.FC = () => {
                     />
                   ))}
                 </div>
-                <button className="submit-button" onClick={handleVerifyOTP} disabled={loadingVerifyOTP}>
+                <button
+                  className="submit-button"
+                  onClick={handleVerifyOTP}
+                  disabled={loadingVerifyOTP}
+                >
                   {loadingVerifyOTP ? "Verifying..." : "Verify OTP"}
                 </button>
               </div>
@@ -226,7 +294,9 @@ const SendCoachEmailOTP: React.FC = () => {
 
             {(errorSendOTP || errorVerifyOTP) && isMobile && (
               <div className="error-message">
-                <p className="error-message-text">{errorSendOTP?.message || errorVerifyOTP?.message}</p>
+                <p className="error-message-text">
+                  {errorSendOTP?.message || errorVerifyOTP?.message}
+                </p>
               </div>
             )}
           </div>
